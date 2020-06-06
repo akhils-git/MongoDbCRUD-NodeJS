b73766ba5623783999ee82b8ef04c95e098d07a4
Akhil Raj <akhilraj@MacBook-Air.local>
2020-06-06 21:01:20 +0530

    Fine tuned

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = new express();
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb+srv://root:1234@iti-dbmyx.mongodb.net/CRUDOprations",
+  "<Add your MongoDB Connection String>",
   { useNewUrlParser: true },
   (err) => {
     if (!err) {
